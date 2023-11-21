45aac826599791dd52063ba9f6be2b0b2729fe99
Mmitov270 <Mmitov270@DESKTOP-QAOS3OF>
2023-11-21 23:59:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.Write("Enter your vehicle registration number: ");
+        string registrationNumber = Console.ReadLine();
+
+        try
+        {
+            bool hasVignette = await CheckVignette(registrationNumber);
+
+            if (hasVignette)
+            {
+                Console.WriteLine("You have a valid vignette.");
+            }
+            else
+            {
+                Console.WriteLine("You do not have a valid vignette.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    static async Task<bool> CheckVignette(string registrationNumber)
+    {
+        // Replace these placeholders with the actual API endpoint and any necessary authentication.
+        string apiUrl = "https://check.bgtoll.bg/";
+        string apiKey = "your_api_key";
+
+        using (HttpClient client = new HttpClient())
+        {
+            // You may need to add headers or parameters based on the API documentation.
+            HttpResponseMessage response = await client.GetAsync($"{apiUrl}?registrationNumber={registrationNumber}&apiKey={apiKey}");
+
+            response.EnsureSuccessStatusCode(); // Throw an exception if the request is not successful.
+
+            // Replace the following line with actual parsing of the response to determine if there's a valid vignette.
+            // This might involve checking the response content or status codes.
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            // For simplicity, let's assume the presence of the word "valid" in the response indicates a valid vignette.
+            return responseBody.ToLower().Contains("valid");
+        }
+    }
+}
